16d63e5f423daec803becd934ecd9e44f5ca74fd
bantylahri95-oss <bantylahri95@gmail.com>
2026-04-03 11:53:40 +0530

    first python programs

---
+++ 17-feb/first.py
@@ -0,0 +1,2 @@
+# this is a comment
+print("Hello, World!")  # this will print a message to the console
---
+++ 17-feb/message.py
@@ -0,0 +1,4 @@
+# This will print a message to the console
+print("Hello, World!")
+print("Welcome to Python programming!")
+print("Let's learn how to print messages in Python.")
